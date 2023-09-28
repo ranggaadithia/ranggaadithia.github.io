c0f23c18657fc9f1ac4f74b1441eba1baf12de56
Rangga Adithia <rangga.adithia26@gmail.com>
2023-09-28 16:23:38 +0800

    edit tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,21 @@ module.exports = {
     enabled: true,
     content: [
       '*.html',
-      './assets/js/main.js'
     ]
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, 
   theme: {
 
     screens: {
       'sm': '540px',
-      // => @media (min-width: 640px) { ... }
 
       'md': '720px',
-      // => @media (min-width: 768px) { ... }
 
       'lg': '960px',
-      // => @media (min-width: 1024px) { ... }
 
       'xl': '1140px',
-      // => @media (min-width: 1280px) { ... }
 
       '2xl': '1320px',
-      // => @media (min-width: 1536px) { ... }
     },
 
     container: {
